2493f5f23df016821650aa806914ba708deaed4a
PrashanthPremchand <prashanthpremchand@gmail.com>
2024-07-17 22:01:27 +0530

    first correction

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         B b = new B();
-        System.out.println(b.meth());
+        B b1 = new B();
+        System.out.println(b1.meth());
     }
 }
