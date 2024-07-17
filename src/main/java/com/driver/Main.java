a23bf2ff9752447c3e99489e492826988a3302d6
PrashanthPremchand <prashanthpremchand@gmail.com>
2024-07-17 21:48:13 +0530

    first correction

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,19 +2,19 @@ package com.driver;
 
 public class Main {
   public static class A{
-      public void meth(){
-          System.out.println("Invoking method from class A");
+      public String meth(){
+          return "Invoking method from class A";
       }
   }
   public static class B extends A{
       @Override
-      public void meth(){
-          System.out.println("Method is overridden in Extended class B");
+      public String meth(){
+          return "Method is overridden in Extended class B";
       }
   }
 
     public static void main(String[] args) {
         B b = new B();
-        b.meth();
+        System.out.println(b.meth());
     }
 }
